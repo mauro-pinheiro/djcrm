20f86ada8ac789e3ad0e0dd4feb9bf0a0044eb55
Mauro Campos <mauro.pcsergio@gmail.com>
2021-02-15 15:46:50 +0000

    lead form

---
+++ leads/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import lead_list, lead_detail, lead_create
+
+urlpatterns = [
+    path('', lead_list),
+    path('create/', lead_create),
+    path('<int:pk>/', lead_detail)
+]
